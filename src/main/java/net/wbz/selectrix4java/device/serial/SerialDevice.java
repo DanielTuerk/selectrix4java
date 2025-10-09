6ce938d7ad03a4e148ac2da417697f9982a91482
Daniel Tuerk <daniel.tuerk@jambit.com>
2025-10-09 23:31:06 +0200

    switch from purejavacomm to jSerialComm to support arm64

--- src/main/java/net/wbz/selectrix4java/device/serial/SerialDevice.java
+++ src/main/java/net/wbz/selectrix4java/device/serial/SerialDevice.java
@@ -1,19 +1,17 @@
 package net.wbz.selectrix4java.device.serial;
 
+import com.fazecast.jSerialComm.SerialPort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.util.Enumeration;
 import net.wbz.selectrix4java.bus.BusDataDispatcher;
 import net.wbz.selectrix4java.data.BusDataChannel;
 import net.wbz.selectrix4java.device.AbstractDevice;
 import net.wbz.selectrix4java.device.DeviceAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import purejavacomm.CommPortIdentifier;
-import purejavacomm.SerialPort;
 
 /**
  * {@link net.wbz.selectrix4java.device.Device} implementation for serial access like COM or USB.
@@ -79,23 +77,20 @@ public class SerialDevice extends AbstractDevice {
     @Override
     protected BusDataChannel doConnect(BusDataDispatcher busDataDispatcher) throws DeviceAccessException {
         System.setProperty("gnu.io.rxtx.SerialPorts", deviceId);
-        Enumeration portList = CommPortIdentifier.getPortIdentifiers();
-        if (portList.hasMoreElements()) {
-            CommPortIdentifier portId = (CommPortIdentifier) portList.nextElement();
-            try {
-                serialPort = (SerialPort) portId.open("net.wbz.selectrix4java", 2000);
-                outputStream = serialPort.getOutputStream();
-                inputStream = serialPort.getInputStream();
-                serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_NONE);
-                serialPort.enableReceiveTimeout(1000);
-                serialPort.setSerialPortParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
-                        SerialPort.PARITY_NONE);
-            } catch (Exception e) {
-                throw new DeviceAccessException(String.format("can't connect to device for id %s", deviceId), e);
-            }
-            return new BusDataChannel(inputStream, outputStream, busDataDispatcher);
+        SerialPort serialPort = SerialPort.getCommPort(deviceId);
+        try {
+            serialPort.openPort();
+            serialPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+            outputStream = serialPort.getOutputStream();
+            inputStream = serialPort.getInputStream();
+            serialPort.setFlowControl(SerialPort.FLOW_CONTROL_DISABLED);
+            serialPort.setBaudRate(baudRate);
+            serialPort.setNumDataBits(8);
+            serialPort.setNumStopBits(1);
+        } catch (Exception e) {
+            throw new DeviceAccessException(String.format("can't connect to device for id %s", deviceId), e);
         }
-        throw new DeviceAccessException(String.format("no device found for id %s", deviceId));
+        return new BusDataChannel(inputStream, outputStream, busDataDispatcher);
     }
 
     /**
@@ -116,8 +111,9 @@ public class SerialDevice extends AbstractDevice {
             }
             outputStream = null;
             inputStream = null;
-            serialPort.removeEventListener();
-            serialPort.close();
+
+            serialPort.closePort();
+            serialPort = null;
         }
     }
 
